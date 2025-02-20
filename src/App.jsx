8294b34ec6cc379e78383c74afe66c1c9c47242e
ahmetkvs <ahmetpkavas@gmail.com>
2025-02-20 18:07:50 +0300

    React18 ve react-router 5.2 uyumsuzlugu cozuldu react router5.3 e gecildi

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { Button } from 'reactstrap';
 import Login from './components/Login'
+import { Switch, Route, BrowserRouter, useHistory } from 'react-router-dom';
+import Success from './components/Success'
+import Error from './components/Error'
 
 function App() {
-  
 
   return (
     <>
-      <Login/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/"><Login/></Route>
+          <Route path="/error"><Error/></Route>
+          <Route path="/success"><Success/></Route> 
+        </Switch>
+      </BrowserRouter>
     </>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,7 +10,7 @@ import {
 import { useHistory } from 'react-router-dom';
 import axios from "axios";
 
-const initalForm = {
+const initialForm = {
     email: '',
     password: '',
     terms: false,
@@ -22,32 +22,108 @@ const errorMessages = {
     At least 8 characters long
     At least one uppercase (English) letter
     At least one lowercase (English) letter
-    At least one digit
-    At least one special charcter`,
+    At least one digit`,
 };
 
-const strongPasswordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+const strongPasswordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/;
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 export default function Login() {
+    let history = useHistory();
+    const [formData, setFormData] = useState(initialForm);
+    const [errors, setErrors] = useState({});
+    const [isValid, setIsValid] = useState({});
 
-    return (
-        <Form>
-            <FormGroup>
-                <Label>Email</Label>
-                <Input></Input>
-            </FormGroup>
-            <FormGroup>
-                <Label>Password</Label>
-                <Input></Input>
-            </FormGroup>
-            <FormGroup check>
-                <Label>I agree to terms of service and privacy policy</Label>
-                <Input type ="checkbox"></Input>
-            </FormGroup>
-            <FormGroup>
-                <Button disabled={true}>Sign in</Button>
-            </FormGroup>
-        </Form>
-    );
+    useEffect(() => {
+        const newErrors = {};
+        if (!emailRegex.test(formData.email)) {
+          newErrors.email = errorMessages.email;
+        }
+        if (!strongPasswordRegex.test(formData.password)){
+          newErrors.password = errorMessages.password;
+        }
+        if (!formData.terms) {
+          newErrors.terms = 'You must accept the terms';
+        }
+        setErrors(newErrors);
+        setIsValid(Object.keys(newErrors).length === 0);
+    }, [formData]);
+
+    const handleChange = (event) => {
+        let { name, value, type, checked } = event.target;
+        value = (type === 'checkbox') ? checked : value;
+        setFormData({...formData, [name]: value});
+        //console.log(value)
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!isValid) return;
+    /*Denemek için
+    "password": "Hs9FVCKFrqf1mdH",
+    "email": "Marcelo89@yahoo.com",
+    */
+        axios 
+          .get('https://6540a96145bedb25bfc247b4.mockapi.io/api/login')
+          .then((res) => {
+            const user = res.data.find(
+              (item) => item.password ===formData.password && item.email === formData.email
+            );
+            if (user) {
+              setFormData(initialForm);
+              history.push('/success');
+            } else {
+              history.push('/error');
+            }
+        });
+    };
+
+        return (
+            <Form onSubmit={handleSubmit}>
+              <FormGroup>
+                <Label for="exampleEmail">Email</Label>
+                <Input
+                  id="exampleEmail"
+                  name="email"
+                  placeholder="Enter your email"
+                  type="email"
+                  onChange={handleChange}
+                  value={formData.email}
+                  invalid={!!errors.email}
+                />
+                {errors.email && <FormFeedback>{errors.email}</FormFeedback>}
+              </FormGroup>
+              <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input
+                  id="examplePassword"
+                  name="password"
+                  placeholder="Enter your password"
+                  type="password"
+                  onChange={handleChange}
+                  value={formData.password}
+                  invalid={!!errors.password}
+                />
+                {errors.password && <FormFeedback>{errors.password}</FormFeedback>}
+              </FormGroup>
+              <FormGroup check>
+                <Input
+                  id="terms"
+                  name="terms"
+                  checked={formData.terms}
+                  type="checkbox"
+                  onChange={handleChange}
+                />{' '}
+                <Label htmlFor="terms" check>
+                  I agree to terms of service and privacy policy
+                </Label>
+                {errors.terms && <div className="text-danger">{errors.terms}</div>}
+              </FormGroup>
+              <FormGroup>
+                <Button disabled={!isValid}>
+                  Sign In
+                </Button>
+              </FormGroup>
+            </Form>
+          );
 }
