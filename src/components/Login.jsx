a63a70d78930e79d9060105ee3e8958284d29905
ahmetkvs <ahmetpkavas@gmail.com>
2025-02-20 16:45:40 +0300

    Login.jsx imported to app.jsx, and visible, fixed issue with reactstrap import

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,32 @@ import {
     Label,
     Input,
     Button,
-    FormFeedBack,
 } from 'reactstrap';
 import { useHistory } from 'react-router-dom';
+import axios from "axios";
 
+const initalForm = {
+    email: '',
+    password: '',
+    terms: false,
+};
+
+const errorMessages = {
+    email: 'please enter a valid email address',
+    password: `Choose a stronger password,
+    At least 8 characters long
+    At least one uppercase (English) letter
+    At least one lowercase (English) letter
+    At least one digit
+    At least one special charcter`,
+};
+
+const strongPasswordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+export default function Login() {
+
+    return (
+        <p>Helloooo</p>
+    );
+}
